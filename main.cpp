2b033d1f75063db6e4707bc6373d1734431bfd21
OSEI <101386110+OSEITD@users.noreply.github.com>
2023-08-14 14:56:35 +0200

    Add files via upload
    best fit memory management using c++

--- main.cpp
+++ main.cpp
@@ -1,30 +1,77 @@
 #include <iostream>
-#include <windows.h>
+#include <cstdlib>
+#include <vector>
+using namespace std;
+
+struct MemoryBlock {
+    int size;
+    bool allocated;
+};
+
+class MemoryManager {
+private:
+    vector<MemoryBlock> memoryBlocks;
+
+public:
+    MemoryManager(vector<int> blockSizes) {
+        for (int size : blockSizes) {
+            memoryBlocks.push_back({size, false});
+        }
+    }
+
+    void bestFitAllocate(int requestedSize) {
+        int bestFitIndex = -1;
+        int bestFitSizeDiff = INT_MAX;
+
+        for (int i = 0; i < memoryBlocks.size(); ++i) {
+            if (!memoryBlocks[i].allocated && memoryBlocks[i].size >= requestedSize) {
+                int sizeDiff = memoryBlocks[i].size - requestedSize;
+                if (sizeDiff < bestFitSizeDiff) {
+                    bestFitSizeDiff = sizeDiff;
+                    bestFitIndex = i;
+                }
+            }
+        }
+
+        if (bestFitIndex != -1) {
+            memoryBlocks[bestFitIndex].allocated = true;
+            cout << "Allocated memory block of size " << requestedSize << endl;
+        } else {
+           cout << "No suitable memory block found for size " << requestedSize << endl;
+        }
+    }
+            cout << " " << endl;
+            
+    void deallocate(int requestedSize) {
+        for (auto& block : memoryBlocks) {
+            if (block.allocated && block.size == requestedSize) {
+                block.allocated = false;
+                cout << "Deallocated memory block of size " << requestedSize << endl;
+                return;
+            }
+        }
+        cout << "Memory block of size " << requestedSize << " not found." << endl;
+    }
+
+    void printMemoryStatus() {
+        for (int i = 0; i < memoryBlocks.size(); ++i) {
+           cout << "Block " << i << ": Size=" << memoryBlocks[i].size << " Allocated=" << (memoryBlocks[i].allocated ? "Yes" : "No") << std::endl;
+        }
+    }
+};
 
 int main() {
-    const int numProcesses = 5;
-    STARTUPINFO si[numProcesses];
-    PROCESS_INFORMATION pi[numProcesses];
+    vector<int> blockSizes = {20, 10, 30, 15};
+    MemoryManager memoryManager(blockSizes);
 
-    for (int i = 0; i < numProcesses; ++i) 
-    {
-        ZeroMemory(&si[i], sizeof(STARTUPINFO));
-        si[i].cb = sizeof(STARTUPINFO);
+    memoryManager.bestFitAllocate(12);
+    memoryManager.bestFitAllocate(25);
+    memoryManager.bestFitAllocate(8);
+    memoryManager.printMemoryStatus();
 
-        CreateProcess(
-            nullptr,                // Module name (use nullptr to run the same program)
-            const_cast<LPSTR>("notepad.exe"), // Command line (executable name)
-            nullptr,                // Process handle not inheritable
-            nullptr,                // Thread handle not inheritable
-            FALSE,     // Set handle inheritance to FALSE
-            0,       // No creation flags
-            nullptr, // Use parent's environment block
-            nullptr, // Use parent's starting directory
-            &si[i], // Pointer to STARTUPINFO structure
-            &pi[i] // Pointer to PROCESS_INFORMATION structure
-        );
-    }
+    memoryManager.deallocate(20);
+    memoryManager.bestFitAllocate(18);
+    memoryManager.printMemoryStatus();
 
     return 0;
 }
-
